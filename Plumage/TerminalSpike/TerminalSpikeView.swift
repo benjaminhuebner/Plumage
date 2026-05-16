8d8ded36bbb4dd69f31ee704ddffb6db850a7b44
Benjamin Hübner <me@benjaminhuebner.com>
2026-05-16 09:58:58 +0200

    add padding around SwiftTerm hosting view

--- Plumage/TerminalSpike/TerminalSpikeView.swift
+++ Plumage/TerminalSpike/TerminalSpikeView.swift
@@ -4,6 +4,8 @@ import SwiftUI
 struct TerminalSpikeView: View {
     var body: some View {
         SwiftTermHostingView()
+            .padding(.horizontal, 10)
+            .padding(.vertical, 8)
             .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
